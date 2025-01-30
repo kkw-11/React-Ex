fa844ebc5717f45ee64c6c7b4afc3089580a4640
kiwon <kkw-11@naver.com>
2025-01-30 12:52:19 +0900

    feat:component test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-import react, {Component} from 'react';
+
+function Header(){
+  return <header>
+    <h1><a href="/">WEB</a></h1>
+  </header>
+}
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        hello react
-      </div>
-    )
-  }
+function Nav() {
+  return <nav>
+  <ol>
+    <li><a href="/read/1">html</a></li>
+    <li><a href="/read/2">css</a></li>
+    <li><a href="/read/3">js</a></li>
+  </ol>
+</nav>
+}
+
+function Article() {
+  return <article>
+    <h2>Welcome</h2>
+      Hello, WEB
+    </article>
+}
+
+function App() {
+  return (
+    <div className='App'>
+      <Header></Header>
+      <Header></Header>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
+    </div>
+  ) 
 }
 
 export default App;
